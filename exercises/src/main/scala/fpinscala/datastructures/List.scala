540c387ef9cf9b47fd17d4ea1f20b7385524dd15
Frank S. Thomas <frank@timepit.eu>
2013-11-14 08:07:41 +0100

    Update List.scala

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -61,9 +61,17 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int =
+    l match {
+      case Nil => 0
+      case Cons(x, xs) => 1 + length(xs)
+    }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
 
   def map[A,B](l: List[A])(f: A => B): List[B] =
     l match {
