7a2877c93d251aa227d8c34b89de8a4fe0d4c4dd
Frank S. Thomas <f.thomas@gmx.de>
2014-09-22 13:23:29 +0200

    Update Option.scala

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -10,7 +10,11 @@ sealed trait Option[+A] {
       case None    => None
     }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case Some(a) => a
+      case None    => default
+    }
 
   def flatMap[B](f: A => Option[B]): Option[B] =
     this match {
@@ -18,9 +22,17 @@ sealed trait Option[+A] {
       case None    => None
     }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this match {
+      case Some(a) => Some(a)
+      case None    => ob
+    }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] =
+    this match {
+      case Some(a) if f(a) => Some(a)
+      case _               => None
+    }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
